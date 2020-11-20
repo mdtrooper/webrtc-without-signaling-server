560b88c4b6548f99d1485e179571e8a2cded8889
Miguel de Dios Matias <tres.14159@gmail.com>
2020-11-20 13:46:36 +0100

    In spanish "guarreando" to look for less code and more clean.

--- offering.js
+++ offering.js
@@ -1,13 +1,107 @@
 function clickcreateoffer() {
-  console.log('clickcreateoffer');
-  document.getElementById('buttoncreateoffer').disabled = true;
-  document.getElementById('spanoffer').classList.toggle('invisible');
-  peerConnection = createPeerConnection(lasticecandidate);
-  dataChannel = peerConnection.createDataChannel('chat');
-  dataChannel.onopen = datachannelopen;
-  dataChannel.onmessage = datachannelmessage;
-  createOfferPromise = peerConnection.createOffer();
-  createOfferPromise.then(createOfferDone, createOfferFailed);
+  //caca();
+  oldcaca();
+  
+  //~ console.log('clickcreateoffer');
+  //~ document.getElementById('buttoncreateoffer').disabled = true;
+  //~ document.getElementById('spanoffer').classList.toggle('invisible');
+  //~ peerConnection = createPeerConnection(lasticecandidate);
+  //~ dataChannel = peerConnection.createDataChannel('chat');
+  //~ dataChannel.onopen = datachannelopen;
+  //~ dataChannel.onmessage = datachannelmessage;
+  //~ createOfferPromise = peerConnection.createOffer();
+  //~ createOfferPromise.then(createOfferDone, createOfferFailed);
+}
+
+function oldcaca() {
+        peerConnection2 = createPeerConnection(
+        function() {
+            console.log('caca lasticecandidate');
+            offer = peerConnection2.localDescription;
+            console.log(JSON.stringify(offer));
+        }
+    );
+    dataChannel = peerConnection2.createDataChannel('chat');
+    dataChannel.onopen = datachannelopen;
+    dataChannel.onmessage = datachannelmessage;
+    createOfferPromise = peerConnection2.createOffer();
+    createOfferPromise.then(
+        function(offer) {
+            console.log('caca createOfferDone');
+            setLocalPromise = peerConnection.setLocalDescription(offer);
+            setLocalPromise.then(
+                function() {
+                    console.log('caca setLocalDone')
+                },
+                function (reason) {
+                    console.log('caca setLocalFailed');
+                    console.log(reason);
+                });
+        },
+        function(reason) {
+            console.log('caca createOfferFailed');
+            console.log(reason);
+        });
+}
+
+function caca() {
+    console.log('caca');
+    
+    let configuration = {
+        iceServers: [{
+            urls: "stun:stun.stunprotocol.org"}]};
+    
+    let peerConnection3 = null;
+    try {
+        console.log('caca new RTCPeerConnection');
+        peerConnection3 = new RTCPeerConnection(configuration);
+    } catch(err) {
+        console.log('caca error: ' + err);
+    }
+    
+    peerConnection3.onicecandidate =
+        function handleicecandidate() {
+            return function(event) {
+                if (event.candidate != null) {
+                    console.log('caca new ice candidate');
+                }
+                else {
+                    console.log('caca all ice candidates');
+                    console.log('caca lasticecandidate');
+                    let offer = peerConnection2.localDescription;
+                    console.log(JSON.stringify(offer));
+                }
+            }
+        }
+    peerConnection3.onconnectionstatechange =
+        function(event) {
+            console.log('caca handleconnectionstatechange');
+            console.log(event);
+        }
+    peerConnection3.oniceconnectionstatechange = function(event) {
+        console.log('caca ice connection state: ' + event.target.iceConnectionState);
+    }
+    dataChannel = peerConnection3.createDataChannel('chat');
+    dataChannel.onopen = datachannelopen;
+    dataChannel.onmessage = datachannelmessage;
+    createOfferPromise = peerConnection3.createOffer();
+    createOfferPromise.then(
+        function(offer) {
+            console.log('caca createOfferDone');
+            setLocalPromise = peerConnection3.setLocalDescription(offer);
+            setLocalPromise.then(
+                function() {
+                    console.log('caca setLocalDone')
+                },
+                function (reason) {
+                    console.log('caca setLocalFailed');
+                    console.log(reason);
+                });
+        },
+        function(reason) {
+            console.log('caca createOfferFailed');
+            console.log(reason);
+        });
 }
 
 function createOfferDone(offer) {
